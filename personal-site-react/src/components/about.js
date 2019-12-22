10bd47b136d7f87972e530eb56c7cab37d82a151
Will Ronchetti <wrr33@cornell.edu>
2019-12-22 12:04:37 -0500

    butcher original structure, define assets, components dirs

---
+++ personal-site-react/src/components/about.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from '../assets/logo.svg';
+import '../assets/App.css';
+
+const About = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to my personal site!
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+}
+
+export default About;
